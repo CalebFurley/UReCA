5fdfbe862fbfd9e7a048506fc3debd46749ff468
Caleb Furley <caleb.furley@gmail.com>
2024-06-12 20:27:28 -0500

    Addition and Print Modules Created and working.

--- print/print.cpp
+++ print/print.cpp
@@ -1,17 +1,35 @@
+#include <pybind11/pybind11.h>
 #include <iostream>
 #include <string>
-#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-void println(std::string message)
+void myPrint(std::string message)
 {
 	std::cout << message << std::endl;
 }
 
-PYBIND11_MODULE(print, m)
+PYBIND11_MODULE(myprint, m)
 {
-	m.def("println", &println, "Prints a message to the console.");
+	m.def("myPrint", &myPrint, "Prints to the console.");
 }
 
-// Follow tutorial and get my python module working here.
+
+
+
+/* ADD FUNCTION IS WRITTEN BELOW
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int myAdd(int x, int y)
+{
+	return x + y;
+}
+
+PYBIND11_MODULE(myadd, m)
+{
+	m.def("myAdd", &myAdd, "Add two numbers.");
+}
+*/
