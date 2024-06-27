675a6bf7ae54f60a5a87c256ab7393f12f413e85
Caleb Furley <caleb.furley@gmail.com>
2024-06-27 13:22:58 -0500

    Built Linear Regression Model. Still needs debugging!

--- models/models.cpp
+++ models/models.cpp
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 
 #include <iostream>
+#include <vector>
 #include <math.h>
 
 namespace py = pybind11;
@@ -15,42 +16,97 @@ namespace py = pybind11;
 class LinearRegression
 {
 private:
-	// Members
+	// Constructor Members
 	Eigen::MatrixXd m_data_X;
 	Eigen::MatrixXd m_data_Y;
-	double m_alpha;
-	int m_epochs;
+	double m_alpha = 0.0;
+	int m_epochs = 0;
+
+	// Training Members
+	Eigen::MatrixXd m_weights;
+	Eigen::MatrixXd m_bias;
+	int m_m = 0;
+	Eigen::MatrixXd m_fwb;
+	double cost = 0.0;
+	Eigen::MatrixXd m_d_weights;
+	double m_d_bias;
+
 
 public:
 	// Constructor
-	LinearRegression(const Eigen::MatrixXd& data_X, const Eigen::MatrixXd& data_Y, double alpha, int epochs)
-		:m_data_X(data_X), m_data_Y(data_Y), m_alpha(alpha), m_epochs(epochs)
+	LinearRegression()
 	{
 		std::cout << "Linear Regression object created" << std::endl;
-		std::cout << "Train X = " << data_X << std::endl;
-		std::cout << "Train Y = " << data_Y << std::endl;
-		std::cout << "Alpha = " << alpha << std::endl;
-		std::cout << "Epochs = " << epochs << std::endl;
 	}
 
 	// Train method
-	void train()
+	void train(const Eigen::MatrixXd& data_X, const Eigen::MatrixXd& data_Y, double alpha, int epochs)
 	{
 		std::cout << "Training Linear Regression model" << std::endl;
+
+		// Initialze data and hyperparameters
+		m_data_X = data_X;
+		m_data_Y = data_Y;
+		m_alpha = alpha;
+		m_epochs = epochs;
+
+		// Initialize weights and bias
+		m_weights = Eigen::VectorXd::Zero(m_data_X.cols());
+		m_bias = Eigen::VectorXd::Zero(1);
+		m_m = m_data_X.rows();
+
+		m_fwb = Eigen::VectorXd::Zero(m_m);
+
+		// Gradient Descent
+		for (int i = 0; i < m_epochs; i++)
+		{
+			// Forward pass
+			m_fwb = (m_data_X * m_weights) + m_bias;
+			cost = (m_fwb - m_data_Y).squaredNorm() / (2 * m_m);
+			
+			// Calculate gradients
+			m_d_weights = (1/m_m) * (m_data_X.transpose() * (m_fwb - m_data_Y));
+			m_d_bias = ((1/m_m) * (m_fwb - m_data_Y)).sum();
+
+			// Update weights and bias
+			m_d_weights = m_d_weights * m_alpha;
+			m_d_bias = m_d_bias * m_alpha;
+
+			for (int j = 0; j < m_weights.size(); j++)
+			{
+				m_weights(j) = m_weights(j) - m_d_weights(j);
+			}
+			m_bias(0) = m_bias(0) - m_d_bias;
+		}
 	}
 
 	// Predict method
 	Eigen::MatrixXd predict(const Eigen::MatrixXd& data_X)
 	{
 		std::cout << "Predicting with Linear Regression model" << std::endl;
-		return data_X;
+
+		// Use trained weights and bias to calculate predictions.
+		Eigen::MatrixXd data_Y = (data_X * m_weights).array() + m_bias(0);
+
+		return data_Y;
 	}
 
 	// Score method
 	double score(const Eigen::MatrixXd& data_X, const Eigen::MatrixXd& data_Y)
 	{
 		std::cout << "Scoring Linear Regression model" << std::endl;
-		return 0.0;
+
+		// Calculate R^2 score
+		Eigen::MatrixXd data_Y_pred = predict(data_X);
+		double ss_res = (data_Y - data_Y_pred).squaredNorm();
+		Eigen::MatrixXd meanAdjusted = data_Y.array() - data_Y.mean();
+		double ss_tot = meanAdjusted.squaredNorm();
+		
+		double epsilon = 1e-8; // Small value to prevent division by zero
+		double r2 = 1 - (ss_res / (ss_tot + epsilon));
+
+
+		return r2;
 	}
 };
 
@@ -59,8 +115,9 @@ public:
 
 PYBIND11_MODULE(models, m)
 {
+	// Expose linear regression class
 	py::class_<LinearRegression>(m, "LinearRegression")
-		.def(py::init<const Eigen::MatrixXd&, const Eigen::MatrixXd&, int, int>())
+		.def(py::init<>())
 		.def("train", &LinearRegression::train)
 		.def("predict", &LinearRegression::predict)
 		.def("score", &LinearRegression::score);
--- python-testing/_test-print.py
+++ python-testing/_test-print.py
@@ -1,12 +1,48 @@
 import numpy as np
-
+import pandas as pd
 from models import LinearRegression
+from sklearn.preprocessing import StandardScaler
 
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y = np.array([[2, 1], [2, 2], [3, 2], [4, 3]])
+#############################################
+############## Import data ##################
+#############################################
+print("Importing Data.")
+df = pd.read_csv('C:/Users/caleb/Code/Trabajo/UReCA/python-testing/simple-housing-data.csv')
+df.head()
+#############################################
 
-reg = LinearRegression(X, y, 1, 500);
-reg.train();
-reg.predict(np.array([[3, 5]]))
-score = reg.score(X, y);
-print(score)
+
+#############################################
+############## Preprocess data ##############
+#############################################
+print("Preprocessing Data.")
+
+# replace all strings with floats
+df = df.replace({'yes':1.0, 'no':0.0, 'furnished':1.0, 'semi-furnished':0.5, 'unfurnished':0.0})
+
+# split up data set into training and testing
+train,test = np.split(df.sample(frac=1),[400],axis=0)
+
+# split training and testing into x and y (use .values)
+train_x, train_y = train.iloc[0:, 1:].values, train['price'].values
+test_x, test_y = test.iloc[0:, 1:].values, test['price'].values
+
+# Scale the features (train_x shows equation, test_x uses Scaler from sklearn)
+scaler = StandardScaler()
+train_x = scaler.fit_transform(train_x)
+test_x = scaler.transform(test_x)  # Note: not fit_transform
+    
+#############################################
+
+
+#############################################
+############## Train model ##################
+#############################################
+print("About to train.\n\n\n")
+
+# Create a Linear Regression model
+model = LinearRegression()
+model.train(train_x, train_y, 0.01, 500)
+model.predict(test_x)
+score = model.score(test_x, test_y)
+print("R^2 Score = ", score)
